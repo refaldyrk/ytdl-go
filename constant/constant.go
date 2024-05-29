8259df13dba1b4b973c993866e15568c4cba6ba1
Refaldy Rizky Karim <88225369+refaldyrk@users.noreply.github.com>
2024-05-29 15:40:17 +0700

    Update constant.go

--- constant/constant.go
+++ constant/constant.go
@@ -1,6 +1,6 @@
 package constant
 
 const (
-	BASE_API_YTDL_ANALYZE  = "https://www.y2mate.com/mates/analyzeV2/ajax"
-	BASE_API_YTDL_DOWNLOAD = "https://www.y2mate.com/mates/convertV2/index"
+	BASE_API_YTDL_ANALYZE  = "https://www.y2mate.com/mates/en948/analyzeV2/ajax"
+	BASE_API_YTDL_DOWNLOAD = "https://www.y2mate.com/mates/en948/convertV2/index"
 )
